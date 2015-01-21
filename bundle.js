7129a0b80f4d19e4c02d94893e6b68ef12feb093
Christoph Herrmann <developer@dragonprojects.de>
2015-01-21 21:27:16 +0100

    Upgrade to DragonNode.js v3.1.0 and use the default values

--- bundle.js
+++ bundle.js
@@ -4,14 +4,8 @@
 
 module.exports = function (config, _, services) {
     config = {
-        npm: __dirname + '/node_modules/',
-        libraries: {
-            nodejs: {},
-            npm: {}
-        },
         directory: __dirname + '/modules/',
         modules: {
-            npm: {},
             directory: config
         }
     };
